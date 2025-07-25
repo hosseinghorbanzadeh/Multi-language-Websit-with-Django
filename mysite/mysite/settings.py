cd3127a3fae1b768db673c213622d204496d706f
hosseinghorbanzadeh <hossein.ghorbanzaeh@gmail.com>
2025-07-25 17:19:42 +0330

    make migrate

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+
 
 TIME_ZONE = 'UTC'
 
@@ -110,6 +110,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
+STATIC_ROOT=BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT=BASE_DIR / 'media'
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
